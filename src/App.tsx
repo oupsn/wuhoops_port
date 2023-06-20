46fa48922a81f39a5e060981ce11598a60ca82e8
Pasinun.w <pasinun.witt@kmutt.ac.th>
2023-06-21 01:09:51 +0700

    [INIT]

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import Lottie from "lottie-react";
 import "./App.css";
 import meditatingfox from "./assets/meditating-fox.json";
-import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
   return (
-    <BrowserRouter>
       <Lottie style={{ height: "40vw" }} animationData={meditatingfox} />
-    </BrowserRouter>
   );
 };
 
