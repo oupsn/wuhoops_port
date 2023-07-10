e1b4a35435a05a3609522b383487e2c7cf3c6bab
Pasinun.w <pasinun.w@gmail.com>
2023-07-10 22:08:03 +0700

    [FIX] nav hero fragment

--- src/component/navBar.tsx
+++ src/component/navBar.tsx
@@ -35,10 +35,9 @@ export default function NavBar() {
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center">
-                  <a href="#" className="block h-8 w-auto lg:hidden">
+                  <a href="#" className="block h-8 w-auto">
                     Wuhoops
                   </a>
-                  <a className="hidden h-8 w-auto lg:block">Wuhoops</a>
                 </div>
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-6">
