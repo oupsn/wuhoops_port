100756d8f1431c83491c14a8cbfbf26a8ec254ad
Pasinun.w <pasinun.w@gmail.com>
2023-07-25 01:50:17 +0700

    [UPDATE] Note found page

--- src/page/notFound.tsx
+++ src/page/notFound.tsx
@@ -1,10 +1,11 @@
 import Lottie from "lottie-react";
-import meditatingfox from "../assets/meditating-fox.json";
+
+import notfound from "../assets/notfound.json";
 const NotFound = () => {
   return (
     <div className="bg-zinc-100 flex flex-col justify-center items-center w-screen h-screen">
-      <div className="max-w-lg mr-10">
-        <Lottie animationData={meditatingfox} />
+      <div className="max-w-lg mr-4">
+        <Lottie animationData={notfound} />
       </div>
       <p className="text-6xl font-semibold mb-20">404 Not Found</p>
     </div>
