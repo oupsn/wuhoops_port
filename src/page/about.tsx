65b341610c7ebc59ca7a2820fc969b7c0f3c21ec
Pasinun.w <pasinun.w@gmail.com>
2023-07-28 20:46:52 +0700

    [FIX] change ,layout

--- src/page/about.tsx
+++ src/page/about.tsx
@@ -7,29 +7,27 @@ const about = () => {
       id="about"
       className="transition-all text-black dark:text-white min-h-screen bg-zinc-100 dark:bg-zinc-900 flex justify-center"
     >
-      <div data-aos="fade">
-        <p className="text-zinc-400 dark:text-zinc-600 text-center -rotate-90 absolute -left-32 hidden lg:block">
-          - No hugs or kisses, only bugs and fixes -
-        </p>
-      </div>
+      
       <div
         data-aos="fade-up"
         className="mt-32 max-w-6xl px-10 flex flex-col gap-20"
       >
-        <Skeleton variant="circular" width={200} height={200} className="self-center ring-2 ring-zinc-600" />
+        <Skeleton variant="circular" width={300} height={300} className="self-center ring-2 ring-zinc-600" />
         <div>
           <p className="text-4xl sm:text-6xl md:text-8xl font-bold text-center mb-4">
             Pasinun Wittayawinit
           </p>
-          <p className="text-xl text-center">
+          {/* <p className="text-xl text-center">
             Currently a undergraduate student of SIT at King Mongkut's
             University of Technology Thonburi (KMUTT).
-          </p>
+          </p> */}
         </div>
         <div>
           <p className="text-2xl font-semibold text-center mb-4">Education</p>
           <EducationTimeline />
         </div>
+        <div>
+      </div>
       </div>
     </div>
   );
