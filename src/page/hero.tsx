241e597a979981704432e2f461251cfcf037430b
Pasinun.w <pasinun.witt@kmutt.ac.th>
2023-08-08 22:52:58 +0700

    [TEST] glitch

--- src/page/hero.tsx
+++ src/page/hero.tsx
@@ -3,7 +3,7 @@ const hero = () => {
     <>
       <div
         id="hero"
-        className="transition-all text-black dark:text-white h-screen bg-zinc-100 dark:bg-zinc-900 flex justify-center items-center"
+        className="transition-all text-black dark:text-white bg-zinc-100 dark:bg-zinc-900 flex justify-center items-center"
       >
         <p data-aos="fade" data-aos-duration="1600" className="text-xl font-medium">
           Hi there, Wu's here.
