1136b21219e54c04a6a82865767133cd258e57ba
Pasinun.w <pasinun.w@gmail.com>
2023-08-09 11:27:57 +0700

    [ADD project swiper for sm

--- src/page/projects.tsx
+++ src/page/projects.tsx
@@ -2,7 +2,16 @@ import PROJECTS from "../assets/projects.ts";
 import { IoIosArrowForward } from "react-icons/io";
 import { Link } from "react-router-dom";
 import StackChips from "../component/stackChips.tsx";
+import { Swiper, SwiperSlide } from "swiper/react";
 
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-cube";
+import "swiper/css/pagination";
+
+import "./styles.css";
+import { EffectCoverflow, Pagination } from "swiper/modules";
+// import required modules
 const projects = () => {
   return (
     <div
@@ -93,6 +102,97 @@ const projects = () => {
             </div>
           </Link>
         </div>
+        <Swiper
+          effect={"coverflow"}
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={"auto"}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          pagination={true}
+          modules={[EffectCoverflow, Pagination]}
+          className="mySwiper"
+        >
+          {PROJECTS.slice(0, 5).map((pj) => {
+            return (
+              <SwiperSlide>
+                <div
+                  key={pj.id}
+                  className="overflow-hidden transition-all group grid grid-rows-2 card max-w-[400px] max-h-[400px] bg-white dark:bg-black ring ring-offset-2 hover:ring-offset-[8px] ring-zinc-300 dark:ring-zinc-600 hover:ring-zinc-600 dark:hover:ring-zinc-300 ring-offset-zinc-100 dark:ring-offset-zinc-900"
+                >
+                  <div className="overflow-hidden p-4 pb-0 h-full w-full">
+                    <div className="rounded-2xl overflow-hidden h-full">
+                      <div className="rounded-2xl overflow-hidden">
+                        <img src={pj.image} alt={pj.alt} />
+                      </div>
+                    </div>
+                  </div>
+                  <div>
+                    <div className="text-black dark:text-white card-body items-center text-center">
+                      <h2 className="card-title">{pj.topic}</h2>
+                      <p className="h-10 text-sm font-thin max-h-10 line-clamp-2 text-ellipsis">
+                        {pj.description}
+                      </p>
+                    </div>
+                    <div className=" px-4 pb-4">
+                      {StackChips({ stacks: pj.stack, isExample: true })}
+                    </div>
+                  </div>
+
+                  <div className="absolute w-full h-full">
+                    <div className="absolute w-full h-full bg-white dark:bg-black transition-all duration-300 scale-0 group-hover:scale-150 rounded-full "></div>
+                    <div className="absolute text-black dark:text-white p-4 w-full h-full transition-all duration-300 scale-0 group-hover:scale-100 rounded-full flex flex-col justify-center items-center">
+                      {pj.res != null ? (
+                        <div>
+                          <p className="font-bold">Responsibilities</p>
+                          {pj.res?.map((res) => {
+                            return (
+                              <p
+                                key={res}
+                                className="text-start self-start"
+                              >{`- ${res}`}</p>
+                            );
+                          })}
+                          <br />
+                          <p className="font-bold mb-2">Tech stack</p>
+                          <div>
+                            {StackChips({ stacks: pj.stack, isExample: false })}
+                          </div>
+                          <br />
+                        </div>
+                      ) : (
+                        ""
+                      )}
+                      <div className="flex gap-4">
+                        <div
+                          onClick={() => window.open(pj.src, "_blank")}
+                          className="bg-black dark:bg-white text-white dark:text-black shadow-md transition-all inline-flex items-center rounded-full px-4 py-2 hover:ring-2 hover:ring-offset-4 hover:ring-offset-zinc-100 dark:hover:ring-offset-zinc-900 hover:ring-black dark:hover:ring-white cursor-pointer"
+                        >
+                          <p>Repository</p>
+                        </div>
+                        {pj.design != null ? (
+                          <div
+                            onClick={() => window.open(pj.design, "_blank")}
+                            className="bg-black dark:bg-white text-white dark:text-black shadow-md transition-all inline-flex items-center rounded-full px-4 py-2 hover:ring-2 hover:ring-offset-4 hover:ring-offset-zinc-100 dark:hover:ring-offset-zinc-900 hover:ring-black dark:hover:ring-white cursor-pointer"
+                          >
+                            <p>UX/UI design</p>
+                          </div>
+                        ) : (
+                          ""
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
       </div>
     </div>
   );
