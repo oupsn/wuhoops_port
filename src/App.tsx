07ddae7c5ec2e7202275a6ad15142515b4eb391d
Pasinun.w <pasinun.witt@kmutt.ac.th>
2023-08-09 23:00:21 +0700

    [ADD] Hero parallax

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,22 @@ import Home from "./page/home";
 import NotFound from "./page/notFound";
 import Backyard from "./page/backyard";
 import { DarkModeProvider } from "./context/darkModeContext";
+import { ParallaxProvider } from "react-scroll-parallax";
 
 function App() {
   return (
     <DarkModeProvider>
-      <React.StrictMode>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/backyard" element={<Backyard />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </React.StrictMode>
+      <ParallaxProvider>
+        <React.StrictMode>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/backyard" element={<Backyard />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </React.StrictMode>
+      </ParallaxProvider>
     </DarkModeProvider>
   );
 }
--- src/page/hero.tsx
+++ src/page/hero.tsx
@@ -1,3 +1,5 @@
+import { Parallax } from "react-scroll-parallax";
+
 const hero = () => {
   return (
     <>
@@ -5,9 +7,32 @@ const hero = () => {
         id="hero"
         className="h-screen transition-all text-black dark:text-white bg-zinc-100 dark:bg-zinc-900 flex justify-center items-center"
       >
-        <p data-aos="fade" data-aos-duration="1600" className="text-xl font-medium">
-          Hi there, Wu's here.
-        </p>
+        <Parallax
+          opacity={[2, 0]}
+          scale={[0, 2]}
+          translateX={["250%", "-250%"]}
+        >
+          <span
+            data-aos="fade"
+            data-aos-duration="1600"
+            className="text-xl font-medium"
+          >
+            Hi there,
+          </span>
+        </Parallax>
+        <Parallax
+          opacity={[2, 0]}
+          scale={[0, 2]}
+          translateX={["-250%", "250%"]}
+        >
+          <span
+            data-aos="fade"
+            data-aos-duration="1600"
+            className="text-xl font-medium"
+          >
+            &nbsp;Wu's here.
+          </span>
+        </Parallax>
       </div>
     </>
   );
