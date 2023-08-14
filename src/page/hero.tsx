30cdbf9a39b608b139cff7e8b476856a86a631ad
Pasinun.w <pasinun.witt@kmutt.ac.th>
2023-08-14 16:36:48 +0700

    [UPDATE] hero wave

--- src/page/hero.tsx
+++ src/page/hero.tsx
@@ -1,29 +1,23 @@
 import { Parallax } from "react-scroll-parallax";
 import "../hero.css";
-import wave from "../assets/wave.svg";
 
 const hero = () => {
   return (
-    <>
-      <div
-        id="hero"
-        className="h-screen transition-all text-black dark:text-white bg-zinc-100 dark:bg-zinc-900 flex justify-center items-center"
-      >
-        <Parallax opacity={[2, 0]} scale={[0, 2]}>
-          <span
-            data-aos="fade"
-            data-aos-duration="1600"
-            className="text-xl font-medium"
-          >
-            Hi there, Wu's here.
-          </span>
-        </Parallax>
-        <div className="ocean">
-          <div className="wave"></div>
-          <div className="wave"></div>
-        </div>
-      </div>
-    </>
+    <div
+      id="hero"
+      className="overflow-hidden h-screen transition-all text-black dark:text-white bg-zinc-100 dark:bg-zinc-900 flex justify-center items-center"
+    >
+      <div className="hero absolute h-screen overflow-hidden dark:invert"></div>
+      <Parallax opacity={[2, 0]} scale={[0, 2]}>
+        <span
+          data-aos="fade"
+          data-aos-duration="1600"
+          className="text-xl font-medium text-blac"
+        >
+          Hi there, Wu's here.
+        </span>
+      </Parallax>
+    </div>
   );
 };
 
